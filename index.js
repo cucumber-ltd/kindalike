0e2178e4ab629eda567d89c5b37ee228314c1944
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-08-21 12:47:45 +0100

    Make usable in browser

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-function kindalike (query, subjects) {
-  var result = [];
-  for(var n in subjects) {
-    var subject = subjects[n];
-    var queryIndex = 0;
-    var subjectIndex = -1;
-    var indices = [];
-    var distance = 0;
+(function () {
+  'use strict';
 
-    // Loop over the chars in the subject
-    while(++subjectIndex < subject.length) {
-      var queryChar = query[queryIndex];
-      var subjectChar = subject[subjectIndex];
-      if(queryChar === subjectChar) {
-        indices.push(subjectIndex);
-        if(queryIndex > 0) {
-          distance += indices[queryIndex] - indices[queryIndex-1] - 1;
+  function kindalike (query, subjects) {
+    var result = [];
+    for(var n in subjects) {
+      var subject = subjects[n];
+      var queryIndex = 0;
+      var subjectIndex = -1;
+      var indices = [];
+      var distance = 0;
+
+      // Loop over the chars in the subject
+      while(++subjectIndex < subject.length) {
+        var queryChar = query[queryIndex];
+        var subjectChar = subject[subjectIndex];
+        if(queryChar === subjectChar) {
+          indices.push(subjectIndex);
+          if(queryIndex > 0) {
+            distance += indices[queryIndex] - indices[queryIndex-1] - 1;
+          }
+          queryIndex++;
         }
-        queryIndex++;
+      }
+      if(indices.length == query.length) {
+        result.push({subject: subject, indices: indices, distance: distance});
       }
     }
-    if(indices.length == query.length) {
-      result.push({subject: subject, indices: indices, distance: distance});
+    result.sort(function(r1, r2) {
+      return r1.distance === r2.distance ? 0 : ( r1.distance < r2.distance ? -1 : 1);
+    });
+    return result;
+  }
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    // Node.js
+    module.exports = kindalike;
+  } else {
+    if (typeof define === 'function' && define.amd) {
+      // AMD
+      define([], function () {
+        return kindalike;
+      });
+    } else {
+      // Browser, no AMD
+      window.kindalike = kindalike;
     }
   }
-  result.sort(function(r1, r2) {
-    return r1.distance === r2.distance ? 0 : ( r1.distance < r2.distance ? -1 : 1);
-  });
-  return result;
-}
-
-module.exports = kindalike;
+})();
