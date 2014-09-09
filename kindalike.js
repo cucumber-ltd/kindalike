4233d2f9e7f04889826566a0c3fa8a236067bbd5
Julien Biezemans <jb@jbpros.com>
2014-09-09 12:43:29 +0200

    Whitespaces

--- kindalike.js
+++ kindalike.js
@@ -1,11 +1,11 @@
 (function () {
   'use strict';
 
-  function kindalike (query, subjects) {
+  function kindalike(query, subjects) {
     query = query.toLowerCase();
 
     var result = [];
-    for(var n in subjects) {
+    for (var n in subjects) {
       var subject = subjects[n];
       var lowerCaseSubject = subject.toLowerCase();
       var queryIndex = 0;
@@ -14,22 +14,22 @@
       var gaps = 0;
 
       // Loop over the chars in the subject
-      while(++subjectIndex < subject.length) {
+      while (++subjectIndex < subject.length) {
         var queryChar = query[queryIndex];
         var subjectChar = lowerCaseSubject[subjectIndex];
-        if(queryChar === subjectChar) {
+        if (queryChar === subjectChar) {
           indices.push(subjectIndex);
-          if(queryIndex > 0) {
+          if (queryIndex > 0) {
             gaps += indices[queryIndex] - indices[queryIndex-1] - 1;
           }
           queryIndex++;
         }
       }
-      if(indices.length == query.length) {
+      if (indices.length == query.length) {
         result.push({subject: subject, indices: indices, gaps: gaps});
       }
     }
-    result.sort(function(r1, r2) {
+    result.sort(function (r1, r2) {
       return r1.gaps === r2.gaps ? 0 : ( r1.gaps < r2.gaps ? -1 : 1);
     });
     return result;
@@ -38,10 +38,10 @@
   /**
    * Turns a match into a string with spans. Special chars are escaped.
    */
-  kindalike.spans = function(match) {
+  kindalike.spans = function (match) {
     var text = '';
     var start = 0;
-    for(var n = 0; n < match.indices.length; n++) {
+    for (var n = 0; n < match.indices.length; n++) {
       var i = match.indices[n];
 
       text += escapeHtml(match.subject.slice(start, i));
